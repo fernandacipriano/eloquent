041c5e5fe63e8c57acc4763e0b093c1658e3e343
Fernanda Cipriano <cipriano632@gmail.com>
2018-06-22 06:14:25 -0300

    primeiro envio

---
+++ database/seeds/ClientsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ClientsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\Models\Client::class,10)
+            ->states(\App\Models\Client::TYPE_INDIVIDUAL)->create();
+        factory(\App\Models\Client::class,10)
+            ->states(\App\Models\Client::TYPE_LEGAL)->create();
+        //
+    }
+}
